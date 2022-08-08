c3a40597ba27690b211f6b95e6d5cb9e0e77ce3c
chocoSprite <chocosprite21@gmail.com>
2022-08-08 16:09:44 +0900

    fix: sync redis for generate sample project

--- generateProject.js
+++ generateProject.js
@@ -34,6 +34,7 @@ const encryptData = (originalMessage) => {
 
 client.on('error', (e) => {
   console.log(e);
+  process.exit();
 });
 client.on('ready', () => {
   console.log(`Redis Connected ${process.env.REDIS}`);
@@ -42,39 +43,53 @@ client.on('ready', () => {
 client
   .connect()
   .then(async () => {
-    const name = readlineSync.question('input your project name : ');
-    const description = readlineSync.question('input your project description : ');
-    const url = readlineSync.question('input your project site URL : ');
-    const icon = readlineSync.question('input your project icon URL : ');
-    const callback = readlineSync.question('input your project sign callback URL : ');
-    const verifyRequest = readlineSync.question('input your project verify request URL : ');
+    try {
+      const name = readlineSync.question('input your project name : ');
+      const description = readlineSync.question('input your project description : ');
+      const url = readlineSync.question('input your project site URL : ');
+      const icon = readlineSync.question('input your project icon URL : ');
+      const callback = readlineSync.question('input your project sign callback URL : ');
+      const verifyRequest = readlineSync.question('input your project verify request URL : ');
 
-    const projectId = uuid.v4();
-    const projectSecretKey = encryptData(projectId);
+      const projectId = uuid.v4();
+      const projectSecretKey = encryptData(projectId);
 
-    client.hSet(projectId, 'name', name);
-    client.hSet(projectId, 'description', description);
-    client.hSet(projectId, 'url', url);
-    client.hSet(projectId, 'icon', icon);
-    client.hSet(projectId, 'callback', callback);
-    client.hSet(projectId, 'verifyRequest', verifyRequest);
+      await client.hSet(projectId, 'name', name);
+      await client.hSet(projectId, 'description', description);
+      await client.hSet(projectId, 'url', url);
+      await client.hSet(projectId, 'icon', icon);
+      await client.hSet(projectId, 'callback', callback);
+      await client.hSet(projectId, 'verifyRequest', verifyRequest);
 
-    console.log({
-      name,
-      description,
-      url,
-      icon,
-      callback,
-      verifyRequest,
-    });
-    console.log(
-      '=========================================================================================================================='
-    );
-    console.log('PROJECT ID : ' + projectId);
-    console.log('PROJECT SECRET KEY : ' + projectSecretKey);
-    console.log(
-      '=========================================================================================================================='
-    );
+      const projectData = {
+        name,
+        description,
+        url,
+        icon,
+        callback,
+        verifyRequest,
+      };
+
+      const getProjectData = await client.hGetAll(projectId);
+
+      for (let key in getProjectData) {
+        if (projectData[key] !== getProjectData[key]) {
+          throw new Error('FAILED NEW PROJECT');
+        }
+      }
+
+      console.log(projectData);
+      console.log(
+        '=========================================================================================================================='
+      );
+      console.log('PROJECT ID : ' + projectId);
+      console.log('PROJECT SECRET KEY : ' + projectSecretKey);
+      console.log(
+        '=========================================================================================================================='
+      );
+    } catch (e) {
+      console.log(e);
+    }
 
     process.exit();
   })
